697ee4451bf841acb7d7cf5a9019ab913dd36be8
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-28 01:36:34 +0300

    implement a basic delete  a genre route

--- app.js
+++ app.js
@@ -55,6 +55,16 @@ app.patch('/api/genres/:id', (req, res) => {
   });
 });
 
+// delete  genere
+app.patch('/api/genres/:id', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      genres: '<updated genre>',
+    },
+  });
+});
+
 // START THE SERVER
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
