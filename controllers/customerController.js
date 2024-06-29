414bf3e1a17ba4030d2dea9ca64435b7064dcfb9
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-30 01:37:34 +0300

    implement get single customer, update customer, delete customer functionalities

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,13 @@
 const Customer = require('./../models/customerModel');
 
-const getAllCustomers = async (req, res, next) => {};
+const getAllCustomers = async (req, res, next) => {
+  const customers = await Customer.find().sort('name');
+  res.status(201).json({
+    status: 'success',
+    data: { customers },
+  });
+};
+
 const createCustomer = async (req, res, next) => {
   const customer = await Customer.create(req.body);
 
@@ -11,9 +18,61 @@ const createCustomer = async (req, res, next) => {
     },
   });
 };
-const getSingleCustomer = async (req, res, next) => {};
-const updateCustomer = async (req, res, next) => {};
-const deleteCustomer = async (req, res, next) => {};
+
+const getSingleCustomer = async (req, res, next) => {
+  const { id } = req.params;
+  const customer = await Customer.findById(id);
+
+  if (!customer)
+    return res.status(404).json({
+      status: 'fail',
+      message: `there is no customer with the ${id}`,
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      customer,
+    },
+  });
+};
+const updateCustomer = async (req, res, next) => {
+  const { id } = req.params;
+  const customer = await Customer.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!customer)
+    return res.status(404).json({
+      status: 'fail',
+      message: `there is no customer with the ${id}`,
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      customer,
+    },
+  });
+};
+const deleteCustomer = async (req, res, next) => {
+  const { id } = req.params;
+  const customer = await Customer.findByIdAndDelete(id, req.body);
+
+  if (!customer)
+    return res.status(404).json({
+      status: 'fail',
+      message: `there is no customer with the ${id}`,
+    });
+
+  res.status(204).json({
+    status: 'success',
+    data: {
+      customer: null,
+    },
+  });
+};
 
 module.exports = {
   getAllCustomers,
